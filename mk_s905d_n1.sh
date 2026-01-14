95e1a572c50915cea966943d2e66f6397b4541c6
BEICHENXIANGHE <992342592@qq.com>
2026-01-14 14:57:15 +0800

    Update mk_s905d_n1.sh

--- mk_s905d_n1.sh
+++ mk_s905d_n1.sh
@@ -86,16 +86,13 @@ SS_LIB="${PWD}/files/ss-glibc/lib-glibc.tar.xz"
 SS_BIN="${PWD}/files/ss-glibc/armv8a_crypto/ss-bin-glibc.tar.xz"
 JQ="${PWD}/files/jq"
 
-# 20210330 add
-DOCKERD_PATCH="${PWD}/files/dockerd.patch"
+
 
 # 20200416 add
 FIRMWARE_TXZ="${PWD}/files/firmware_armbian.tar.xz"
 BOOTFILES_HOME="${PWD}/files/bootfiles/amlogic"
 GET_RANDOM_MAC="${PWD}/files/get_random_mac.sh"
 
-# 20210618 add
-DOCKER_README="${PWD}/files/DockerReadme.pdf"
 
 # 20210704 add
 SYSINFO_SCRIPT="${PWD}/files/30-sysinfo.sh"
@@ -167,7 +164,6 @@ echo "修改根文件系统相关配置 ... "
 cd $TGT_ROOT
 copy_supplement_files
 extract_glibc_programs
-adjust_docker_config
 adjust_openssl_config
 adjust_getty_config
 adjust_samba_config
