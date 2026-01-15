6f372f55c33270dddfd92bb8a8d1d1bef887b683
BEICHENXIANGHE <992342592@qq.com>
2026-01-15 14:06:54 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # 添加包
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+# git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
